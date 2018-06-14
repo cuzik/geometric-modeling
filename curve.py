0b491705190a8c2ac7b28bb3ec5f47851ae6e49e
cuzik <cuzikjack@gmail.com>
2018-06-14 15:57:45 -0300

    refc

--- curve.py
+++ curve.py
@@ -2,77 +2,103 @@ import os
 import numpy as np
 import scipy.special
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, K_SPACE
+
 
 class Curve(object):
-  """docstring for Curve"""
-  def __init__(self, color_points, color_curve, screen):
-    super(Curve, self).__init__()
-    self.color_points = color_points
-    self.color_curve = color_curve
-    self.screen = screen
-    self.define_points_done = False
-    self.curve_points = []
-    self.control_points = []
-    self.define_control_points()
-    self.define_curve_ponits()
-    self.draw_all_struct()
+    """docstring for Curve"""
 
-  def define_curve_ponits(self):
-    self.curve_points = []
+    def __init__(self, color_points, color_curve, screen):
+        super(Curve, self).__init__()
+        self.color_points = color_points
+        self.color_curve = color_curve
+        self.screen = screen
+        self.define_points_done = False
+        self.curve_points = []
+        self.control_points = []
+        self.define_control_points()
+        self.define_curve_ponits()
+        self.draw_all_struct()
 
-  def draw_points(self):
-    for point in self.control_points:
-      pygame.draw.circle(self.screen, self.color_points, point, 4)
+    def define_curve_ponits(self):
+        self.curve_points = []
 
-  def draw_edge(self):
-    for i in range(0,len(self.control_points)-1):
-      pygame.draw.lines(self.screen, self.color_points, False , [self.control_points[i], self.control_points[i+1]], 1)
+    def draw_points(self):
+        for point in self.control_points:
+            pygame.draw.circle(self.screen, self.color_points, point, 4)
 
-  def draw_curve(self):
-    pygame.draw.lines(self.screen, self.color_curve, False, self.curve_points, 3)
+    def draw_edge(self):
+        for i in range(0, len(self.control_points) - 1):
+            pygame.draw.lines(
+                self.screen,
+                self.color_points,
+                False,
+                [self.control_points[i], self.control_points[i + 1]],
+                1,
+            )
 
-  def draw_all_struct(self):
-    self.draw_points()
-    self.draw_edge()
-    self.draw_curve()
+    def draw_curve(self):
+        pygame.draw.lines(self.screen, self.color_curve, False, self.curve_points, 3)
 
-  def get_events(self):
-    for event in pygame.event.get():
-      if event.type == QUIT:
-        quit()
-      elif event.type == KEYDOWN and event.key == K_ESCAPE:
-        quit()
-      elif event.type == pygame.MOUSEBUTTONUP:
-        self.control_points += [pygame.mouse.get_pos()]
-      elif event.type == KEYDOWN and event.key == K_SPACE:
-        self.define_points_done = True
+    def draw_all_struct(self):
+        self.draw_points()
+        self.draw_edge()
+        self.draw_curve()
 
-  def define_control_points(self):
-    while not self.define_points_done:
-      self.get_events()
-      self.draw_points()
-      self.draw_edge()
-      pygame.display.flip()
+    def get_events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit()
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                quit()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.control_points += [pygame.mouse.get_pos()]
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                self.define_points_done = True
 
-  def move(self, reference_point):
-    diference_point = (self.control_points[0][0] - reference_point[0], self.control_points[0][1] - reference_point[1])
-    for i in range(0,len(self.control_points)):
-      self.control_points[i] = (self.control_points[i][0] - diference_point[0], self.control_points[i][1] - diference_point[1])
-    self.define_curve_ponits()
+    def define_control_points(self):
+        while not self.define_points_done:
+            self.get_events()
+            self.draw_points()
+            self.draw_edge()
+            pygame.display.flip()
 
-  def last_control_point(self):
-    return self.control_points[-1]
+    def move(self, reference_point):
+        diference_point = (
+            self.control_points[0][0] - reference_point[0],
+            self.control_points[0][1] - reference_point[1],
+        )
+        for i in range(0, len(self.control_points)):
+            self.control_points[i] = (
+                self.control_points[i][0] - diference_point[0],
+                self.control_points[i][1] - diference_point[1],
+            )
+        self.define_curve_ponits()
 
-  def get_coefficient(self):
-    if self.control_points[1][0] - self.control_points[0][0] == 0:
-      return 1
-    return (self.control_points[1][1] - self.control_points[0][1]) / float  (self.control_points[1][0] - self.control_points[0][0])
+    def last_control_point(self):
+        return self.control_points[-1]
 
-  def change_penultimate_control_point(self, coefficient):
-    self.control_points[-2] = (self.control_points[-2][0], int(self.control_points[-1][1] + (self.control_points[-2][0] - self.control_points[-1][0]) * coefficient))
-    self.define_curve_ponits()
+    def get_coefficient(self):
+        if self.control_points[1][0] - self.control_points[0][0] == 0:
+            return 1
+        return (self.control_points[1][1] - self.control_points[0][1]) / float(
+            self.control_points[1][0] - self.control_points[0][0]
+        )
 
-  def aa(self):
-    self.control_points[-2] = (self.control_points[-1][0] * 2 - self.control_points[-2][0], self.control_points[-1][1] * 2 - self.control_points[-2][1])
-    self.define_curve_ponits()
+    def change_penultimate_control_point(self, coefficient):
+        self.control_points[-2] = (
+            self.control_points[-2][0],
+            int(
+                self.control_points[-1][1]
+                + (self.control_points[-2][0] - self.control_points[-1][0])
+                * coefficient
+            ),
+        )
+        self.define_curve_ponits()
+
+    def aa(self):
+        self.control_points[-2] = (
+            self.control_points[-1][0] * 2 - self.control_points[-2][0],
+            self.control_points[-1][1] * 2 - self.control_points[-2][1],
+        )
+        self.define_curve_ponits()
--- main.py
+++ main.py
@@ -2,52 +2,55 @@ import os
 import numpy as np
 import scipy.special
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE, K_0, K_1, K_a, K_c
 
 from curve import Curve
 from bezier import Bezier
 from nurbs import Nurbs
 
-os.environ['SDL_VIDEO_CENTERED'] = '1'
+os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
-screen = pygame.display.set_mode((0,0), pygame.RESIZABLE)
-pygame.display.set_caption('Modelagem Geometrica TOP')
+screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+pygame.display.set_caption("Modelagem Geometrica TOP")
 screen.fill((22, 22, 22))
 
 game_run = True
 only_curve = False
 
 bezier_curve = Bezier([150, 0, 0], [200, 200, 0], screen)
-nurbs_curve  = Nurbs([0, 150, 0], [0, 200, 200], screen)
+nurbs_curve = Nurbs([0, 150, 0], [0, 200, 200], screen)
+
 
 def get_events_draw():
-  global game_run
-  global only_curve
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      game_run = False
-    elif event.type == KEYDOWN and event.key == K_ESCAPE:
-      game_run = False
-    elif event.type == KEYDOWN and event.key == K_0:
-      nurbs_curve.move(bezier_curve.last_control_point())
-    elif event.type == KEYDOWN and event.key == K_1:
-      bezier_curve.change_penultimate_control_point(nurbs_curve.get_coefficient())
-    elif event.type == KEYDOWN and event.key == K_a:
-      bezier_curve.aa()
-    elif event.type == KEYDOWN and event.key == K_c:
-      only_curve = only_curve == False
+    global game_run
+    global only_curve
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            game_run = False
+        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+            game_run = False
+        elif event.type == KEYDOWN and event.key == K_0:
+            nurbs_curve.move(bezier_curve.last_control_point())
+        elif event.type == KEYDOWN and event.key == K_1:
+            bezier_curve.change_penultimate_control_point(nurbs_curve.get_coefficient())
+        elif event.type == KEYDOWN and event.key == K_a:
+            bezier_curve.aa()
+        elif event.type == KEYDOWN and event.key == K_c:
+            only_curve = only_curve == False
+
 
 def main():
-  while game_run:
-    get_events_draw()
-    screen.fill((22, 22, 22))
-    if only_curve:
-      bezier_curve.draw_curve()
-      nurbs_curve.draw_curve()
-    else:
-      bezier_curve.draw_all_struct()
-      nurbs_curve.draw_all_struct()
-    pygame.display.flip()
+    while game_run:
+        get_events_draw()
+        screen.fill((22, 22, 22))
+        if only_curve:
+            bezier_curve.draw_curve()
+            nurbs_curve.draw_curve()
+        else:
+            bezier_curve.draw_all_struct()
+            nurbs_curve.draw_all_struct()
+        pygame.display.flip()
 
-if __name__ == '__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
